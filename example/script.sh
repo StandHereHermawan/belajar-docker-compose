378ba9598ae1ff2938c79c698e63cd28d97524bc
Arief-Karditya <nolifegansaya@gmail.com>
2024-02-08 11:26:38 +0700

    Stop Running Container, Compose Ops

--- example/script.sh
+++ example/script.sh
@@ -6,3 +6,7 @@ docker compose start
 
 # LIST Running Container based on config file command
 docker compose ps
+
+# STOP Running Container based on config file command
+docker compose stop
+# Tidak akan menghapus container yang telah dibuat
