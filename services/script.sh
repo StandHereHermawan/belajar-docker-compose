1e7e24cbac729a17cd30f11ffefc3f234a3c7c46
Arief-Karditya <nolifegansaya@gmail.com>
2024-02-08 11:52:06 +0700

    Service, Compose Ops

---
+++ services/script.sh
@@ -0,0 +1,12 @@
+# CREATE Container using config file command
+docker compose create
+
+# START Container using config file command
+docker compose start
+
+# LIST Running Container based on config file command
+docker compose ps
+
+# REMOVE Container, Volume and Network based on config file command
+docker compose down
+# menghapus semua Container, Volume and Network based on config file.
