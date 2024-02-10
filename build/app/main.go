6d7d3050f873d50c310dc76739fbf2bfb1357c5f
Arief-Karditya <nolifegansaya@gmail.com>
2024-02-10 11:37:13 +0700

    Dockerfile, Compose Create Image Ops

---
+++ build/app/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("APP_PORT")
+	fmt.Println("Run in app port : " + port)
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":"+port, nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello! This is Docker Compose world.")
+	fmt.Fprintln(w, "Arief currently learn Docker Compose.")
+}
---
+++ build/script.sh
@@ -0,0 +1,5 @@
+# BUILD IMAGE only docker compose command
+docker compose build
+
+# REMOVE builded image using compose are manually.
+# docker compose down command doesnt delete image
